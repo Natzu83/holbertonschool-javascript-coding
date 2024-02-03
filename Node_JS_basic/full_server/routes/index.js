d37c240bdea2ec7eec5b47619d2904a25bdf2b34
Natzu83 <natzuac@yahoo.com>
2024-02-02 23:33:48 -0400

    Spaces

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,1 +1,14 @@
-Placeholder
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+// Define a function 'mapRoutes' that takes an 'app' object as a parameter.
+const mapRoutes = (app) => {
+  // Map routes to controller methods by defining HTTP GET endpoints.
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
+
+// Export the 'mapRoutes' function to be used elsewhere.
+export default mapRoutes;
+module.exports = mapRoutes;
